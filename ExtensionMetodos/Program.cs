ec17510031e2c723668011e8019dfffee2b67623
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-09-12 15:56:00 -0600

    Agregar archivos de proyecto.

---
+++ ExtensionMetodos/Program.cs
@@ -0,0 +1,14 @@
+﻿var people = new Person[]
+{
+    new Person { Name = "Leticia", Age = 30 },
+    new Person { Name = "Rodrigo", Age = 18 },
+    new Person { Name = "Ernesto", Age = 17 },
+    new Person { Name = "Tania", Age = 24 },
+    new Person { Name = "Pedro", Age = 21 }
+};
+var adults = people.FilterByAge(21);
+
+foreach (Person person in adults)
+{
+    Console.WriteLine($"{person.Name}, Age: {person.Age}");
+}
